184afe6a03288b8bc9c387560ebad7452793d78b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-23 22:05:01 +0100

    Added code to seeds.sql to populate table with data

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT 0,
+    devoured BOOLEAN NOT NULL DEFAULT 'false',
     PRIMARY KEY (id) 
 );
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES 
+    ("Cheeseburger", "false"),
+    ("Double Cheeseburger", "false"),
+    ("Bacon Double Cheeseburger", "false");
