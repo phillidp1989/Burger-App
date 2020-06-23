3a7c23d9395cd74979cfff0613d0d6a6ea9e87e6
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-23 23:21:47 +0100

    Setup of methods in orm.js file and exported

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,35 @@
 const { connection } = require("./config/connection");
+
+// Promisify connection.query method when querying data from database
+const queryAsync = util.promisify(connection.query).bind(connection);
+
+// Setup of the ORM
+const orm = {
+  selectAll: async function (tableInput) {
+    try {
+      const queryString = `SELECT * FROM ${tableInput}`;
+      await queryAsync(queryString);
+    } catch (err) {
+      console.log("ERROR - orm.js - selectAll(): ", err);
+    }
+  },
+  insertOne: async function (tableInput, columnName, value) {
+    try {
+      const queryString = `INSERT INTO ?? (??) VALUES (?)`;
+      await queryAsync(queryString, [tableInput, columnName, value]);
+    } catch (err) {
+      console.log("ERROR - orm.js - insertOne(): ", err);
+    }
+  },
+  updateOne: async function (tableInput, columnName, newVal, referenceCol, referenceVal) {
+    try {
+      const queryString = `UPDATE ?? SET ?? = ? WHERE ?? = ?`;
+      await queryAsync(queryString, [tableInput, columnName, newVal, referenceCol, referenceVal]);
+    } catch (err) {
+      console.log("ERROR - orm.js - insertOne(): ", err);
+    }
+  },
+
+};
+
+module.exports = orm;
