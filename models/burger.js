5301eb873a58bbd11132c9923095a072c982f60f
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-26 22:49:13 +0100

    Added method to burger model to delete record

--- models/burger.js
+++ models/burger.js
@@ -3,7 +3,8 @@ const orm = require("../config/orm");
 const burger = {
     all: async () => await orm.selectAll("burgers"),
     create: async value => await orm.insertOne("burgers", "burger_name", value),
-    update: async (newVal, referenceVal) => await orm.updateOne("burgers", "devoured", newVal, "id", referenceVal)    
+    update: async (newVal, referenceVal) => await orm.updateOne("burgers", "devoured", newVal, "id", referenceVal),
+    delete: async referenceVal => await orm.deleteOne("burgers", "id", referenceVal)    
 };
 
 module.exports = burger;
