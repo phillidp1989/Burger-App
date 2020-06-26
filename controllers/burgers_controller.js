234460f180fa11f2a2e30ebe0ec892cf5fbb42f3
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-26 11:46:18 +0100

    Routes setup in controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const burger = require("../models/burger");
+const { end } = require("../config/connection");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const data = await burger.all();
-    const burgerObject = { burger: data };
-    res.render("index", burgerObject);
-  } catch (err) {
-    console.log("ERROR - burger_controller.js - router.get(/): ", err);
-  }
+    try {
+        const data = await burger.all();
+        const burgerObject = { burger: data };
+        res.render("index", burgerObject);
+    } catch (err) {
+        console.log("ERROR - burger_controller.js - router.get(/): ", err);
+    }
 });
+
+router.post("/api/burger", async (req, res) => {
+    try {
+        const newBurger = req.body.burgerName;
+        const data = await burger.create(newBurger);
+        if (data.affectedRows === 0) {
+            res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    } catch (err) {
+        console.log("ERROR - burger_controller.js - router.post(/api/burger): ", err);
+    }
+});
+
+router.put("/api/burger/:id", async (req, res) => {
+    try {
+        const burgerId = req.params.id;
+        const data = await burger.update(true, burgerId);
+        if (data.changedRows === 0) {
+            res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    
+    } catch (err) {
+        console.log("ERROR - burger_controller.js - router.put(/api/burger/:id): ", err);
+}
+})
 
 module.exports = router;
